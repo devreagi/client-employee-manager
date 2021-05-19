0a2501f1ba62ea942d532aff7f019f718779970f
devreagi <ivan.reyesa@outlook.com>
2021-05-19 16:22:33 -0500

    Add employee model and update service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Employee} from '../models/employee';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,27 @@ export class EmployeeService {
   constructor(private http: HttpClient) {
   }
 
-  public getEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(
+      `${this.apiServerUrl}/employee/add`,
+      employee,
+    );
+  }
+
+  public deleteEmployee(employeeId: Employee): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/employee/delete/${employeeId}`,
+    );
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(
+      `${this.apiServerUrl}/employee/update`,
+      employee,
+    );
   }
 }
